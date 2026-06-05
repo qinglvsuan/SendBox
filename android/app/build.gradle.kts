7b1a79e394a62f8efa7f359e9d474b303459fff6
qinglvsuan <qinglvsuan@example.com>
2026-06-06 00:11:34 +0800

    Fix Android signature (use local debug keystore) to allow in-place updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,11 +25,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("sendbox.jks")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
